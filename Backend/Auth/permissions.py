d1cf33093c82043dc598ce02d34c8fba7554b9ad
ghost <aldynobruel0gmail.com>
2025-04-18 00:52:41 +0300

    Bug après avoir mis en place les permissions et les groups

--- Backend/Auth/permissions.py
+++ Backend/Auth/permissions.py
@@ -1,4 +1,8 @@
 from rest_framework import permissions
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from Treatment.models import Treatment
+from ContactPro.models import Slot, Appointement
 
 class IsDoctor(permissions.BasePermission):
     """
@@ -13,3 +17,43 @@ class IsPatient(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and not request.user.is_doctor 
+
+def setup_permissions():
+    patient_group, _ = Group.objects.get_or_create(name='Patients')
+    doctor_group, _ = Group.objects.get_or_create(name='Doctors')
+    
+    treatment_content_type = ContentType.objects.get_for_model(Treatment)
+    slot_content_type = ContentType.objects.get_for_model(Slot)
+    appointment_content_type = ContentType.objects.get_for_model(Appointement)
+
+    patient_permissions = [
+        'view_treatment',
+        'create_treatment',
+        'change_treatment',
+        'delete_treatment',
+        'view_doctor_list',
+        'reserve_slot',
+        'view_available_slots',
+        'view_patient_appointement',
+    ]
+    
+    doctor_permissions = [
+        'create_slot',
+        'delete_slot',
+        'change_slot',
+        'view_doctor_appointement',
+    ]
+    
+    for codename in patient_permissions:
+        permission = Permission.objects.get(
+            codename=codename,
+            content_type= treatment_content_type
+        )
+        patient_group.permissions.add(permission)
+    
+    for codename in doctor_permissions:
+        permission = Permission.objects.get(
+            codename=codename,
+            content_type=treatment_content_type
+        )
+        doctor_group.permissions.add(permission)
--- Backend/ContactPro/apps.py
+++ Backend/ContactPro/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ContactPro'
+
+    def ready(self):
+        from Auth.permissions import setup_permissions
+        setup_permissions()
